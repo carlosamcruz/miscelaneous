117b7a45464f999f357a48da5b9c9607df8f4681
carlosamcruz <carlosamcruz@ufam.edu.br>
2023-07-03 14:56:58 -0300

    GPToken New

--- generaltoken/interactGT03OrderLock.ts
+++ generaltoken/interactGT03OrderLock.ts
@@ -17,26 +17,33 @@ import { buildPublicKeyHashScript, sha256, PubKey, toHex} from 'scrypt-ts'
 
     await GeneralToken.compile()
 
-    const privateKey2 = bsv.PrivateKey.fromHex("ccbf6b06cb35e102f42ecfaa5de55ab6cd35431b95f6ee4fd5199ad90943c5cf", bsv.Networks.testnet)
-    //const privateKey2 = bsv.PrivateKey.fromHex('79342a4c317817a80a298fe116147a74e4e90912a4f321e588a4db67204e29b0', bsv.Networks.testnet)
+    //const privateKey2 = bsv.PrivateKey.fromHex("ccbf6b06cb35e102f42ecfaa5de55ab6cd35431b95f6ee4fd5199ad90943c5cf", bsv.Networks.testnet)
+    const privateKey2 = bsv.PrivateKey.fromHex('79342a4c317817a80a298fe116147a74e4e90912a4f321e588a4db67204e29b0', bsv.Networks.testnet)
+
+    let pbkeyAlice = bsv.PublicKey.fromPrivateKey(privateKey2)
 
     //Preferential Buyer 
     //const privateKeyX = bsv.PrivateKey.fromHex('b8b9a38e0bb59b09abf02f1e616d16fe060a74be3957519f64505ca090938f95', bsv.Networks.testnet)
+    //Pub Key: 02d37b073bd0bfb45670404abe683f13907584b979bda1552d9cce901c1b9ff5be
+    //let pbkeyUserX = bsv.PublicKey.fromHex('02d37b073bd0bfb45670404abe683f13907584b979bda1552d9cce901c1b9ff5be')
 
     //No Preferential Buyer - Owner's PK
-    const privateKeyX = bsv.PrivateKey.fromHex('79342a4c317817a80a298fe116147a74e4e90912a4f321e588a4db67204e29b0', bsv.Networks.testnet)
-    
+    //const privateKeyX = bsv.PrivateKey.fromHex('79342a4c317817a80a298fe116147a74e4e90912a4f321e588a4db67204e29b0', bsv.Networks.testnet)
+    //Pub Key: 03c0e0bf0bbcdc53be9542359aeb1dde7c6289743b7b3460c12e2d57a478c6e489
+    let pbkeyUserX = bsv.PublicKey.fromHex('03c0e0bf0bbcdc53be9542359aeb1dde7c6289743b7b3460c12e2d57a478c6e489')
+
+
+    console.log('PUB Key: ', toHex(pbkeyUserX))
   
     // Prepare signer.
     // See https://scrypt.io/docs/how-to-deploy-and-call-a-contract/#prepare-a-signer-and-provider
     const Alice = new TestWallet(privateKey2, new DefaultProvider({network: bsv.Networks.testnet}))
-
     let provDf = new DefaultProvider({network: bsv.Networks.testnet})
 
     await provDf.connect()
     let tx3 = new bsv.Transaction
 
-    tx3 = await provDf.getTransaction('36d2cb0a6f849dd6d7073707208fb897838c50c8970545e8174dca783682a19f')
+    tx3 = await provDf.getTransaction('6e1af8f9b7a83234a343b592ec8326762e63cc3847ce50f2b56f9096a31615ef')
 
     let orderCancel = false
 
@@ -46,9 +53,6 @@ import { buildPublicKeyHashScript, sha256, PubKey, toHex} from 'scrypt-ts'
     let instance2 = GeneralToken.fromTx(tx3, posNew1)
     //Inform to the system the right output index of the contract and its sequence
     tx3.pvTxIdx(tx3.id, posNew1, sha256(tx3.outputs[posNew1].script.toHex()))
-
-    let pbkeyAlice = bsv.PublicKey.fromPrivateKey(privateKey2)
-    let pbkeyUserX = bsv.PublicKey.fromPrivateKey(privateKeyX)
 
     let pvtkey = privateKey2;
     let pbkey = pbkeyAlice;
